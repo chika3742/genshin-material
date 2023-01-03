e75f11e76686328f74daa573cf4aa4df420e30b7
chika3742 <kazu.chika.shima@gmail.com>
2023-01-03 19:35:35 +0900

    Modify: app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtLoadingIndicator
+        :duration="3000"
+        color="linear-gradient(to right, #4FC3F7, #D4E157, #FFA726)"
+      />
+      <NuxtPage :page-key="$route.fullPath" />
+    </NuxtLayout>
   </div>
 </template>
