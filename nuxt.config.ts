042ea4a6e45af5ba7edc37e7c207b1db7f3c27bc
chika3742 <kazu.chika.shima@gmail.com>
2023-01-04 15:46:18 +0900

    Add: i18n config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,4 +28,32 @@ export default defineNuxtConfig({
   css: [
     "assets/styles/global.sass",
   ],
+
+  i18n: {
+    locales: [
+      {
+        code: "ja",
+        iso: "ja-JP",
+        file: "ja.yaml",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en.yaml",
+      },
+    ],
+    langDir: "./locales/",
+    defaultLocale: "ja",
+    vueI18n: {
+      legacy: false,
+      fallbackLocale: "ja",
+      datetimeFormats: {
+        ja: {
+          time: {
+            hour12: false,
+          },
+        },
+      },
+    },
+  },
 })
