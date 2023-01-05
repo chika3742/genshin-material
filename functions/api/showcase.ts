cec137900327de7ed560137bfe88a857569034b3
chika3742 <kazu.chika.shima@gmail.com>
2023-01-05 23:53:40 +0900

    Add: Pages FunctionsでキャラショーケースAPIを実装

---
+++ functions/api/showcase.ts
@@ -0,0 +1,87 @@
+import {EnkaNetworkResponse, ErrorResponse} from "~types/showcase"
+import loc from "~libs/data/loc.json"
+import characterIds from "~libs/data/character-ids.json"
+import travelerSkillSetIds from "~libs/data/traveler-skill-set-ids.json"
+
+function errorResponse(message: string, status: number) {
+  return jsonResponse({
+    errorMessage: message,
+  } as ErrorResponse, {
+    status,
+  })
+}
+
+function jsonResponse(json: Record<string, any>, init?: ResponseInit) {
+  return new Response(JSON.stringify(json), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...init,
+  })
+}
+
+export const onRequest: PagesFunction = async(context) => {
+  if (context.request.method !== "GET") {
+    return errorResponse("Method not allowed", 405)
+  }
+
+  const requestUrl = new URL(context.request.url)
+
+  if (!requestUrl.searchParams.has("uid")) {
+    return errorResponse("Parameter 'uid' is required", 400)
+  }
+
+  const result = await fetch(`https://enka.network/u/${requestUrl.searchParams.get("uid")}/__data.json`, {
+    headers: {
+      "User-Agent": "Genshin Material Data Sync/2.0.0 (gms.chikach.net)",
+    },
+  })
+  const data = await result.json<EnkaNetworkResponse>()
+
+  if (!data.playerInfo.showAvatarInfoList || !data.avatarInfoList ||
+    data.playerInfo.showAvatarInfoList.length === 0) {
+    return errorResponse(
+      "キャラクターラインナップの詳細が公開されていないか、1体も設定されていません",
+      204,
+    )
+  }
+
+  return jsonResponse({
+    uid: data.uid,
+    ttl: data.ttl,
+    username: data.playerInfo.nickname,
+    adventureRank: data.playerInfo.level,
+    showcaseCharacters: data.playerInfo.showAvatarInfoList.map((e, index) => {
+      const skillLevels = Object.values(data.avatarInfoList[index].skillLevelMap)
+      if (skillLevels.length === 4) {
+        skillLevels.splice(0, 1)
+      }
+
+      let characterName = characterIds[e.avatarId]
+
+      if (characterName === "Traveler") {
+        characterName = travelerSkillSetIds[data.avatarInfoList[index].skillDepotId]
+      }
+
+      const weaponData = data.avatarInfoList[index].equipList.find(e => e.weapon)
+      return {
+        characterId: e.avatarId,
+        characterName,
+        level: e.level,
+        ascension: parseInt(data.avatarInfoList[index].propMap["1002"].val ?? "0"),
+        talentLevels: {
+          normal: skillLevels[0],
+          skill: skillLevels[1],
+          burst: skillLevels[2],
+        },
+        weapon: weaponData
+          ? {
+              nameJP: loc[weaponData.flat.nameTextMapHash],
+              level: weaponData.weapon!.level,
+              ascension: weaponData.weapon!.promoteLevel ?? 0,
+            }
+          : undefined,
+      }
+    }),
+  })
+}
---
+++ types/showcase.ts
@@ -0,0 +1,54 @@
+export interface ShowcaseCharacterWeapon {
+  nameJP: string
+  level: number
+  ascension: number
+}
+
+export interface ShowcaseCharacter {
+  characterId: string
+  characterName: string
+  level: number
+  ascension: number
+  talentLevels: {
+    normal: number
+    skill: number
+    burst: number
+  }
+  weapon: ShowcaseCharacterWeapon | undefined
+}
+
+interface PropMap {
+  type: number
+  val: string
+}
+
+export interface EnkaNetworkResponse {
+  uid: string
+  ttl: number
+  playerInfo: {
+    nickname: string
+    level: number
+    showAvatarInfoList: {
+      avatarId: string
+      level: number
+    }[]
+  }
+  avatarInfoList: {
+    propMap: { [id: string]: PropMap }
+    skillLevelMap: { [id: string]: number }
+    skillDepotId: number
+    equipList: {
+      weapon?: {
+        level: number
+        promoteLevel?: number
+      }
+      flat: {
+        nameTextMapHash: string
+      }
+    }[]
+  }[]
+}
+
+export interface ErrorResponse {
+  errorMessage: string
+}
