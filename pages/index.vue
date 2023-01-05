18fcbea078cc037ace54394b79d04d83a76875a3
chika3742 <kazu.chika.shima@gmail.com>
2023-01-05 19:12:45 +0900

    Add: pages/index.vue

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <PageContainer>
+    <h1 class="font-kaisei-opti mb-4">
+      {{ $t("common.app_name") }}
+    </h1>
+    <p>
+      原神の素材収集メモなど、原神を楽しむ上で更に便利になるツールです。
+    </p>
+  </PageContainer>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  title: "home",
+})
+</script>
