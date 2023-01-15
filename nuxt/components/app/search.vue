67e592e2d17e4e9ca5329db868f98c14c632bbe9
chika3742 <kazu.chika.shima@gmail.com>
2023-01-15 16:48:45 +0900

    検索ダイアログのレイアウト調整

--- nuxt/components/app/search.vue
+++ nuxt/components/app/search.vue
@@ -1,33 +1,32 @@
 <template>
   <v-dialog :model-value="modelValue" height="700px" max-width="500px" @update:model-value="$emit('update:modelValue', $event)">
-    <v-card>
-      <v-text-field
-        v-model="searchText"
-        :label="$t('common.search')"
-        :messages="$t('common.search_field_message')"
-        autofocus
-        class="ma-4"
-        prepend-icon="mdi-magnify"
-      />
-      <v-list>
-        <v-list-item
-          v-for="(hit, i) in searchResults"
-          :key="i"
-          :prepend-icon="getIcon(hit.type)"
-          :subtitle="$t(`pages.${hit.type}`)"
-          :title="locale === 'ja' ? hit.nameJP : hit.nameEN"
-          :to="localePath(`/${hit.type}/${hit.id}`)"
-          lines="two"
-          link
-          @click="$emit('update:modelValue', false)"
-        />
-      </v-list>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn flat @click="$emit('update:modelValue', false)">
-          {{ $t('common.close') }}
-        </v-btn>
-      </v-card-actions>
+    <v-card height="100%">
+      <div class="h-100 d-flex flex-column">
+        <v-row class="flex-grow-0" no-gutters>
+          <v-text-field
+            v-model="searchText"
+            :label="$t('common.search')"
+            :messages="$t('common.search_field_message')"
+            autofocus
+            class="ma-4"
+            prepend-icon="mdi-magnify"
+          />
+          <v-btn class="ma-2" density="comfortable" icon="mdi-close" variant="text" @click="$emit('update:modelValue', false)" />
+        </v-row>
+        <v-list>
+          <v-list-item
+            v-for="(hit, i) in searchResults"
+            :key="i"
+            :prepend-icon="getIcon(hit.type)"
+            :subtitle="$t(`pages.${hit.type}`)"
+            :title="locale === 'ja' ? hit.nameJP : hit.nameEN"
+            :to="localePath(`/${hit.type}/${hit.id}`)"
+            lines="two"
+            link
+            @click="$emit('update:modelValue', false)"
+          />
+        </v-list>
+      </div>
     </v-card>
   </v-dialog>
 </template>
