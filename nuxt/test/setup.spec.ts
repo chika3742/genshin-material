e8363503e8ade74d6e420acf7196424aa88ed24a
chika3742 <kazu.chika.shima@gmail.com>
2024-01-12 00:09:47 +0900

    test: setup

---
+++ nuxt/test/setup.spec.ts
@@ -0,0 +1,25 @@
+import {beforeAll, describe, expect, it} from "vitest"
+import {execa} from "execa"
+import {getRandomPort, waitForPort} from "get-port-please"
+import {createContext, createPage} from "~/test/test-utils"
+
+const port = await getRandomPort()
+beforeAll(async() => {
+  console.log("Building...")
+  await execa("yarn", ["generate"])
+  console.log("Starting server...")
+  void execa("npx", ["serve", "dist", "-l", port.toString()])
+  await waitForPort(port, {retries: 20})
+
+  createContext({
+    port,
+  })
+}, 120 * 1e3)
+
+describe("setup", () => {
+  it("should be set up", async() => {
+    const index = await createPage("/")
+
+    expect(await index.waitForSelector(".v-application")).toBeTruthy()
+  })
+})
